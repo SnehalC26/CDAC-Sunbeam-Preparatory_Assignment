a9189516aa5ca03f358c1100b8304270f9191989
SnehalC26 <sb.chougule01@gmail.com>
2026-08-30 09:07:46 +0530

    Added q14 string palindrome

---
+++ Q14StringPalindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class StringPalindrome {
+
+    void checkPalindrome(String str) {
+
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            reversed = reversed + str.charAt(i);
+        }
+
+        if (str.equals(reversed))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
+
+public class Q14StringPalindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        StringPalindrome sp = new StringPalindrome();
+        sp.checkPalindrome(str);
+
+        sc.close();
+    }
+}
